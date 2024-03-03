f5772d288d0e2f42cb479eaa621c74d5aab55c02
Lexe <ngocle.ng1001@gmail.com>
2024-03-03 13:16:49 +0700

    feat: implement recipes-list component UI

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipes } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrl: './recipes-list.component.css'
+  styleUrl: './recipes-list.component.css',
 })
 export class RecipesListComponent {
-
+  recipes: Recipes[] = [
+    new Recipes(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+    new Recipes(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+  ];
 }
