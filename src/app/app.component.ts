448e6d264d73b8819473e9c7ab3912548d24373e
Lexe <ngocle.ng1001@gmail.com>
2024-03-11 00:52:20 +0700

    feat: implement navigate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
+type Feature = 'recipe' | 'shopping-list';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  loadedFeature: Feature = 'recipe';
+
+  handleNavigate(newFeature: Feature) {
+    this.loadedFeature = newFeature;
+  }
 }
