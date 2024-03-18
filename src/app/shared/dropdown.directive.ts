58d10904de333264d81fb707bc46d2b4b6801116
Lexe <ngocle.ng1001@gmail.com>
2024-03-18 22:04:55 +0700

    feat: create directive to toggle dropdown

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,20 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Renderer2,
+} from '@angular/core';
+
+@Directive({ selector: '[appDropdown]' })
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen: boolean;
+
+  constructor(private elRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
+  }
+}
