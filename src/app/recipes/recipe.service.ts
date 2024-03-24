0a06f5a4ce9a2873cdaf3d264d0e34ea5f4f81a6
Lexe <ngocle.ng1001@gmail.com>
2024-03-24 14:40:33 +0700

    feat: implement ingredients to recipe detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -8,12 +9,15 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 2)]
     ),
     new Recipe(
       'Another Test Recipe 2',
       'This is simply a test',
-      'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg'
+      'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg',
+      [new Ingredient('Buns', 1), new Ingredient('Meat', 2)]
+
     ),
   ];
 
